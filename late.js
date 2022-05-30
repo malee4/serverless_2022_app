c6384481a08f5757f9e2bab9a8c6016f4aeef493
malee4 <melody.lee357@gmail.com>
2022-05-30 12:55:30 -0400

    temp

--- late.js
+++ late.js
@@ -1,6 +1,6 @@
 // start coding your function here!
 function running_late(date) {
-    let hour = date.hour;
+    let hour = date.getHours;
     if (hour >= 22) {
         return 'It is late!';
     }
@@ -8,6 +8,8 @@ function running_late(date) {
         return 'It is still early!'
     }
 }
-
+var friend = new Date(1995, 12, 17, 22, 24, 0);
+console.log(friend.getHours)
+//console.log(running_late(friend))
 
 exports.running_late = running_late();
