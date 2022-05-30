1df4305a6a3c6b1853c5175233d737915c022087
malee4 <melody.lee357@gmail.com>
2022-05-30 12:42:56 -0400

    Add content to the late.js file

--- late.js
+++ late.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+function running_late(date) {
+    let hour = date.getHours();
+    if (hour >= 22) {
+        return 'It is late!';
+    }
+    else {
+        return 'It is still early!'
+    }
+}
+
+
+exports.running_late = running_late;
