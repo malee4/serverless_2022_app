70fea988104ae2e4ec4118ed79edf20de153c680
malee4 <melody.lee357@gmail.com>
2022-05-31 09:27:45 -0400

    assume date is a string

--- late.js
+++ late.js
@@ -1,12 +1,16 @@
 // start coding your function here!
-function running_late(date = new Date()) {
-    let hour = date.getHours();
-    if (hour >= 22) {
-        return 'It is late!';
+function running_late(date = "") {
+    let temp = date.split(" ");
+    let hour = temp[3].substring(0,2);
+    if (hour >= "22") {
+        "It is late!";
     }
     else {
-        return 'It is still early!'
+        return "It is still early!";
     }
 }
+// let date = "December 17, 1995 03:24:00";
+
+
 
 exports.running_late = running_late();
